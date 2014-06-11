e423ae59b262d403fb567f3109484f96ab57683f
Stephen Li <stephen.li@lookout.com>
2014-06-11 11:30:21 -0700

    Added primary keys to database export

--- data/intern_project.sql
+++ data/intern_project.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2014 at 06:16 PM
+-- Generation Time: Jun 11, 2014 at 06:29 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.4.24
 
@@ -27,12 +27,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `addresses` (
+`id` int(11) NOT NULL,
   `owner_id` int(11) NOT NULL,
   `type` int(1) NOT NULL,
   `street_address` varchar(200) NOT NULL,
   `city` varchar(100) NOT NULL,
   `country` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -41,8 +42,9 @@ CREATE TABLE IF NOT EXISTS `addresses` (
 --
 
 CREATE TABLE IF NOT EXISTS `companies` (
+`id` int(11) NOT NULL,
   `name` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -51,9 +53,10 @@ CREATE TABLE IF NOT EXISTS `companies` (
 --
 
 CREATE TABLE IF NOT EXISTS `housing` (
+`id` int(11) NOT NULL,
   `price` decimal(10,0) NOT NULL DEFAULT '0',
   `source` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -62,12 +65,13 @@ CREATE TABLE IF NOT EXISTS `housing` (
 --
 
 CREATE TABLE IF NOT EXISTS `ratings` (
+`id` int(11) NOT NULL,
   `owner_id` int(11) NOT NULL,
   `type` int(1) NOT NULL,
   `rating` int(100) NOT NULL,
   `review` varchar(1000) NOT NULL,
   `created` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- Indexes for dumped tables
@@ -77,8 +81,50 @@ CREATE TABLE IF NOT EXISTS `ratings` (
 -- Indexes for table `addresses`
 --
 ALTER TABLE `addresses`
- ADD KEY `owner_id` (`owner_id`);
+ ADD PRIMARY KEY (`id`), ADD KEY `owner_id` (`owner_id`);
 
+--
+-- Indexes for table `companies`
+--
+ALTER TABLE `companies`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `housing`
+--
+ALTER TABLE `housing`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ratings`
+--
+ALTER TABLE `ratings`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `addresses`
+--
+ALTER TABLE `addresses`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `companies`
+--
+ALTER TABLE `companies`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `housing`
+--
+ALTER TABLE `housing`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ratings`
+--
+ALTER TABLE `ratings`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
