8ce3e9c44b535c6312f42c4ac042dca2f103a248
Louisa Chong <louisachong@louisachong.com>
2014-06-11 17:06:13 -0700

    Add bootstrap files and have base map working

--- django/server/urls.py
+++ django/server/urls.py
@@ -1,7 +1,11 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
 
 urlpatterns = patterns('',
     (r'^$', TemplateView.as_view(template_name="index.html")),
+    (r'^statics/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_URL}),
+
 )
