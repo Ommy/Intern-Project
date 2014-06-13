cc801d6f3eb1b1b134f72bb6220017cd49563250
Stephen Li <stephenli.ca@gmail.com>
2014-06-13 02:33:14 -0700

    Added heatmap

--- django/statics/js/app.js
+++ django/statics/js/app.js
@@ -1,7 +1,4 @@
 (function() {
-    housePoints = [];
-    heatMapData = [];
-
     google.load('jquery', '1.4.2');
     google.load('jqueryui', '1.7.2');
     google.load('maps', '3', { other_params: 'key=AIzaSyDvrDCSfQxN9h5UCwLKJxrb5kBQyI7l0cc&libraries=visualization' });
@@ -90,24 +87,91 @@
         });
     };
 
+    var previousQuery = {
+        distance: {
+            min: -1,
+            max: -1
+        },
+        priceRange: {
+            min: -1,
+            max: -1
+        },
+        safety: -1
+    };
+
     closeSidebar = function() {
+        $('#loader').fadeIn();
+
+        var minDistance   = $('#distance-input').attr('data-min');
+        var maxDistance   = $('#distance-input').attr('data-max');
+        var minPriceRange = $('#price-range-input').attr('data-min');
+        var maxPriceRange = $('#price-range-input').attr('data-max');
+        var safety        = $('#safety').val();
+
+        // wtf...
+        if (minDistance == previousQuery.distance.min &&
+            maxDistance == previousQuery.distance.max &&
+            minPriceRange == previousQuery.priceRange.min &&
+            maxPriceRange == previousQuery.priceRange.max &&
+            safety == previousQuery.safety) {
+            $('#st-container').removeClass('st-menu-open');
+            $('#loader').fadeOut();
+            return;
+        } else {
+            previousQuery.distance.min = minDistance;
+            previousQuery.distance.max = maxDistance;
+            previousQuery.priceRange.min = minPriceRange;
+            previousQuery.priceRange.max = maxPriceRange;
+            previousQuery.safety = safety;
+        }
+
         $.ajax({
-            url:        '/api/map',
+            url:        '/api/weights',
             type:       'GET',
             dataType:   'json',
             data: {
                 distance: {
-                    min: $('#distance-input').attr('data-min'),
-                    max: $('#distance-input').attr('data-max')
+                    min: minDistance,
+                    max: maxDistance
                 },
                 price: {
-                    min: $('#price-range-input').attr('data-min'),
-                    max: $('#price-range-input').attr('data-max')
+                    min: minPriceRange,
+                    max: maxPriceRange
                 },
-                safety:  $('#safety').val()
+                safety:  safety
             },
             success: function(data) {
-                console.log(data);
+                $('#loader').fadeOut();
+                var heatMapData = [];
+                for (var i = data.length - 1; i >= 0; i--) {
+                    var point = data[i];
+                    heatMapData.push({
+                        location: new google.maps.LatLng(parseFloat(point.latitude), parseFloat(point.longitude)),
+                        weight:   parseFloat(point.weight)
+                    });
+                };
+                var heatmap = new google.maps.visualization.HeatmapLayer({
+                    data:    heatMapData,
+                    map:     window.map,
+                    radius:  100,
+                    gradient: [
+                        'rgba(0,   255, 255, 0)',
+                        'rgba(0,   255, 255, 1)',
+                        'rgba(0,   191, 255, 1)',
+                        'rgba(0,   127, 255, 1)',
+                        'rgba(0,   63,  255, 1)',
+                        'rgba(0,   0,   255, 1)',
+                        'rgba(0,   0,   223, 1)',
+                        'rgba(0,   0,   191, 1)',
+                        'rgba(0,   0,   159, 1)',
+                        'rgba(0,   0,   127, 1)',
+                        'rgba(63,  0,   91,  1)',
+                        'rgba(127, 0,   63,  1)',
+                        'rgba(191, 0,   31,  1)',
+                        'rgba(255, 0,   0,   1)'
+                    ].reverse()
+                });
+                console.log(heatMapData);
             },
             error: function(xhr, textStatus, errorThrown) {
                 console.log(textStatus);
