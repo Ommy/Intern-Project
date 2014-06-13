438a729c777e5f6401c9e0c3a4a5d1fb6948f39d
Louisa Chong <louisa.chong@lookout.com>
2014-06-12 20:27:27 -0700

    Working Job, House and Crime API

--- django/app/serializers.py
+++ django/app/serializers.py
@@ -1,21 +1,46 @@
 from django.forms import widgets
 from rest_framework import serializers
-from app.models import Companies, Crimes, Addresses
+from app.models import Address, Company, Crime, House, Job, JobRating, JobSalary
 
-
-class CompanySerializer(serializers.ModelSerializer):
-	address = AddressSerializer()
-    class Meta:
-        model = Companies
-        fields = ('id', 'name', 'address')
-
-class CrimeSerilizer(serializers.ModelSerializer):
-	address = AddressSerializer()
-	class Meta:
-		model = Crimes
-		fields = ('id', 'occurred', 'address', 'occurred', 'description')
 
 class AddressSerializer(serializers.ModelSerializer):
 	class Meta: 
-		model = Addresses
+		model = Address
 		fields = ('street_address', 'city',  'country', 'longitude', 'latitude')
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ('id', 'name')
+
+class CrimeSerializer(serializers.ModelSerializer):
+	address = AddressSerializer()
+	class Meta:
+		model = Crime
+		fields = ('id', 'address', 'incident_num', 'occurred', 'category', 'description')
+
+class HouseSerializer(serializers.ModelSerializer):
+	address = AddressSerializer()
+	class Meta:
+		model = House
+		fields = ('id', 'address', 'price', 'source')
+
+class JobSerializer(serializers.ModelSerializer):
+	company = CompanySerializer()
+	address = AddressSerializer()
+	class Meta:
+		model = Job
+		fields = ('id', 'company', 'address', 'title', 'description')
+
+class JobRatingSerializer(serializers.ModelSerializer):
+	job = JobSerializer()
+	class Meta:
+		model = JobRating
+		fields = ('id', 'job', 'source', 'review', 'created')
+
+class JobSalarySerializer(serializers.ModelSerializer):
+	job = JobSerializer()
+	class Meta:
+		model = JobRating
+		fields = ('id', 'job', 'amount')
+		
--- django/app/urls.py
+++ django/app/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('app.views',
     url(r'^companies/$', 'company_list'),
-    url(r'^crimes/$', 'crime_list')
+    url(r'^crimes/$', 'crime_list'),
+    url(r'^houses/$', 'house_list'),
+    url(r'^jobs/$', 'job_list')
 )
