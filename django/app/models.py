d3094cd40ae63b5ecab944479bff7a76222f5d3f
Louisa Chong <louisa.chong@lookout.com>
2014-06-12 18:06:18 -0700

    Update model with DB migrate

--- django/app/models.py
+++ django/app/models.py
@@ -1,51 +1,79 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Remove `managed = False` lines if you wish to allow Django to create and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+#
+# Also note: You'll have to insert the output of 'django-admin.py sqlcustom [appname]'
+# into your database.
+from __future__ import unicode_literals
+
 from django.db import models
 
-class Addresses(models.Model):
-    id = models.IntegerField(primary_key=True)
-    owner_id = models.IntegerField()
-    type = models.IntegerField()
+class Address(models.Model):
+    id = models.IntegerField()
     street_address = models.CharField(max_length=200, blank=True)
     city = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     longitude = models.DecimalField(max_digits=18, decimal_places=12, blank=True, null=True)
     latitude = models.DecimalField(max_digits=18, decimal_places=12, blank=True, null=True)
     class Meta:
-        db_table = 'addresses'
+        managed = False
+        db_table = 'address'
 
-class Companies(models.Model):
-    id = models.IntegerField(primary_key=True)
+class Company(models.Model):
+    id = models.IntegerField()
     name = models.CharField(max_length=200)
     class Meta:
-        db_table = 'companies'
+        managed = False
+        db_table = 'company'
 
-class Housing(models.Model):
-    id = models.IntegerField(primary_key=True)
-    price = models.DecimalField(max_digits=10, decimal_places=0)
+class Crime(models.Model):
+    id = models.IntegerField()
+    address = models.ForeignKey(Address)
+    incident_num = models.IntegerField(unique=True)
+    occurred = models.DateTimeField()
+    category = models.CharField(max_length=200, blank=True)
+    description = models.CharField(max_length=200, blank=True)
+    class Meta:
+        managed = False
+        db_table = 'crime'
+
+class House(models.Model):
+    id = models.IntegerField()
+    address = models.ForeignKey(Address)
+    price = models.DecimalField(max_digits=9, decimal_places=0)
     source = models.IntegerField()
     class Meta:
-        db_table = 'housing'
+        managed = False
+        db_table = 'house'
 
-class Jobs(models.Model):
-    id = models.IntegerField(primary_key=True)
-    company = models.ForeignKey(Companies)
+class Job(models.Model):
+    id = models.IntegerField()
+    company = models.ForeignKey(Company)
+    address = models.ForeignKey(Address)
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     class Meta:
-        db_table = 'jobs'
+        managed = False
+        db_table = 'job'
 
-class Ratings(models.Model):
-    id = models.IntegerField(primary_key=True)
-    owner_id = models.IntegerField()
-    type = models.IntegerField()
-    rating = models.IntegerField(blank=True, null=True)
+class JobRating(models.Model):
+    id = models.IntegerField()
+    job = models.ForeignKey(Job)
+    score = models.IntegerField(blank=True, null=True)
     review = models.TextField(blank=True)
     created = models.CharField(max_length=100, blank=True)
     class Meta:
-        db_table = 'ratings'
+        managed = False
+        db_table = 'job_rating'
 
-class Salaries(models.Model):
+class JobSalary(models.Model):
     id = models.IntegerField(primary_key=True)
-    job = models.ForeignKey(Jobs)
+    job = models.ForeignKey(Job)
     amount = models.DecimalField(max_digits=9, decimal_places=2)
     class Meta:
-        db_table = 'salaries'
+        managed = False
+        db_table = 'job_salary'
+
